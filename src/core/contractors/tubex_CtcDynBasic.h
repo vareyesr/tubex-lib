9fe56eb52caeeb8d26ad31677c187162f9f3e2cd
victor <victor@Legend>
2020-05-18 14:18:09 +0200

    DynBasic: fixed

--- src/core/contractors/tubex_CtcDynBasic.h
+++ src/core/contractors/tubex_CtcDynBasic.h
@@ -23,7 +23,6 @@ namespace tubex
 
 	public:
 
-		CtcDynBasic(tubex::Fnc& fnc2, double prec = 0.);
 		CtcDynBasic(tubex::Function& fnc, double prec = 0.);
 		/*
 		 * This method performs a contraction for the TubeVector x.
@@ -31,14 +30,9 @@ namespace tubex
 		 */
 		bool contract(std::vector<Slice*> x_slice, std::vector<Slice*> v_slice, TPropagation t_propa);
 		/*
-		 * Only for non IVPs,BVPs
-		 */
-		bool contract(std::vector<Slice*> x_slice, std::vector<Slice*> v_slice, TubeVector& x ,int slice_id, TPropagation t_propa);
-		/*
 		 * ctc_fwd manages to make an evaluation of the current Slices in order to contract and update v
 		 */
 		void ctc_fwd(Slice &x, Slice &v, std::vector<Slice*> x_slice, std::vector<Slice*> v_slice, int pos);
-		void ctc_fwd(Slice &x, Slice &v, std::vector<Slice*> x_slice, std::vector<Slice*> v_slice, TubeVector& aux_vector_x,int slice_id, int pos);
 		/*
 		 *  used to obtain the current precision of the iterative method.
 		 */
@@ -48,18 +42,18 @@ namespace tubex
 		 */
 		void set_prec(double prec);
 
+		bool get_reasoning_slice();
+
 		void set_reasoning_slice(bool reasoning_slice = true);
 
+
+
 	private:
+
 		bool m_reasoning_slice = true;
 		CtcDeriv ctc_deriv;
 		tubex::Function& fnc;
 		double prec;
-		//for integrodiff todo: make everything general..
-		tubex::Fnc& fnc2;
-		TubeVector tube_x;
-		int slice_id;
-
 	};
 }
 
